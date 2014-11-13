fe9fd8e3de2e9b62ca70fea26d6050e002b60c97
Oren Nachman <git@nachmore.com>
2014-11-13 15:45:53 -0800

    Added various user warnings
    1. Warn the user that we are not 100% that this is safe to use
    2. Let the user know that we collect analytics

--- src/unBand/App.xaml.cs
+++ src/unBand/App.xaml.cs
@@ -13,5 +13,9 @@ namespace unBand
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            unBand.Properties.Settings.Default.Save();
+        }
     }
 }
--- src/unBand/MainWindow.xaml.cs
+++ src/unBand/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using unBand.pages;
+using MahApps.Metro.Controls.Dialogs;
+using System.Diagnostics;
 
 namespace unBand
 {
@@ -25,19 +27,77 @@ namespace unBand
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!Properties.Settings.Default.AgreedToFirstRunWarning)
+            {
+                if (!await AgreeToFirstRunWarning())
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+
+            if (!Properties.Settings.Default.AgreedToAnalytics)
+            {
+                if (!await AgreeToAnalytics())
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+            
             ButtonMyBand_Click(null, null);
+        }
+
+        private async Task<bool> AgreeToFirstRunWarning()
+        {
+            var dialogSettings = new MetroDialogSettings();
+            dialogSettings.AffirmativeButtonText = "Understood";
+            dialogSettings.NegativeButtonText = "Exit";
+
+            var dialogResult = await this.ShowMessageAsync("Hello", 
+                "WARNING: This software is not supported by Microsoft, we take no responsibility for what may happen to your Band.\n\n... and no, we haven't seen anything bad... yet.", 
+                MessageDialogStyle.AffirmativeAndNegative, dialogSettings);
+
+            if (dialogResult == MessageDialogResult.Affirmative)
+            {
+                Properties.Settings.Default.AgreedToFirstRunWarning = true;
+                return true;
+            }
+
+            return false;
+        }
+
+        private async Task<bool> AgreeToAnalytics()
+        {
+            var dialogSettings = new MetroDialogSettings(); 
+            dialogSettings.AffirmativeButtonText = "I agree";
+            dialogSettings.NegativeButtonText = "Exit";
+
+            var dialogResult = await this.ShowMessageAsync("Heads Up",
+                "We track usage of certain portions of this application, such as section usage and failure information. We don't collect anything personal (ever), but you should still know about it.\n\n", 
+                MessageDialogStyle.AffirmativeAndNegative, dialogSettings);
+
+            if (dialogResult == MessageDialogResult.Affirmative)
+            {
+                Properties.Settings.Default.AgreedToAnalytics = true;
+                return true;
+            }
+
+            return false;
         }
 
         private void ButtonMyBand_Click(object sender, RoutedEventArgs e)
         {
             PageContent.Content = new MyBandPage();
         }
-        
+
         private void ButtonTheme_Click(object sender, RoutedEventArgs e)
         {
             PageContent.Content = new ThemePage();
         }
-        
+
         private void ButtonSensors_Click(object sender, RoutedEventArgs e)
         {
             PageContent.Content = new SensorsPage();
@@ -46,6 +106,11 @@ namespace unBand
         public void Navigate(UserControl content)
         {
             PageContent.Content = content;
+        }
+
+        private void MetroWindow_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.Save();
         }
     }
 }
