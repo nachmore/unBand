1640d219ce0efd4e3f784ba400eed4026a6080b0
Oren Nachman <git@nachmore.com>
2014-12-03 14:41:42 -0800

    Added OppositeBoolConverter
    Useful for RadioButton binding

---
+++ src/unBand/OppositeBoolConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace unBand
+{
+    class OppositeBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool? source = value as bool?;
+
+            if (source == null)
+            {
+                return true;
+            }
+
+            return (!(bool)source);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
