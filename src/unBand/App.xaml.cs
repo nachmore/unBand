94792535512270a9bdf17e7c21e36aed9f9d7740
Oren Nachman <git@nachmore.com>
2014-12-04 00:22:29 -0800

    Add visual MessageBox when an unhandled crash is encountered

--- src/unBand/App.xaml.cs
+++ src/unBand/App.xaml.cs
@@ -21,6 +21,9 @@ namespace unBand
         private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
             Telemetry.Client.TrackException(e.Exception);
+
+            MessageBox.Show("An unhandled exception occurred - sorry about that, we're going to have to crash now :(\n\nYou can open a bug with a copy of this crash: hit Ctrl + C right now and then paste into a new bug at https://github.com/nachmore/unBand/issues.\n\n" + e.Exception.ToString(),
+                "Imminent Crash", MessageBoxButton.OK, MessageBoxImage.Exclamation);
         }
     }
 }
