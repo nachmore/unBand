0f46ab4e9d8abeffd462ad1e549c7195f16f6397
Oren Nachman <orena@microsoft.com>
2014-12-15 16:44:54 -0800

    Add feedback link to About page

--- src/unBand/pages/AboutPage.xaml.cs
+++ src/unBand/pages/AboutPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,12 @@ namespace unBand.pages
             InitializeComponent();
 
             this.DataContext = About.Current;
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
