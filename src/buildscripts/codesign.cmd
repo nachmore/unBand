80d9bc993521ccf1a47794fb39d982e9e54b4d80
Oren Nachman <git@nachmore.com>
2014-12-30 01:54:49 -0800

    Added code signing build step

---
+++ src/buildscripts/codesign.cmd
@@ -0,0 +1,10 @@
+@set /p input=Enter Private Key cert password:
+
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin\signtool" sign /t http://timestamp.digicert.com /f %1 /p "%input%" %2
+
+@IF NOT ERRORLEVEL 1 goto Exit
+
+@pause
+
+:Exit
+
