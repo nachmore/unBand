0a32a90f46a2d1f42f66ef11e9e3ef26872fb309
Oren Nachman <git@nachmore.com>
2015-01-05 22:44:04 -0800

    Hide event details pane when there is no event
    * Added NullToVisibilityConverter
    * Added null check to PropValueToVisibilityConverter

--- src/unBand/Converters/NullToVisibilityConverter.cs
+++ src/unBand/Converters/NullToVisibilityConverter.cs
@@ -8,11 +8,11 @@ using System.Windows.Data;
 
 namespace unBand
 {
-    class PropValueToVisibilityConverter : IValueConverter
+    class NullToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            return (value != null && value.ToString() == parameter.ToString() ? Visibility.Visible : Visibility.Collapsed);
+            return (value == null ? Visibility.Collapsed : Visibility.Visible);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
