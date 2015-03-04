1d829caf34bf4760b7cf633d849e11b6443dbd19
Oren Nachman <git@nachmore.com>
2015-03-03 19:13:31 -0800

    Added a pretty message with the workaround for "name cannot have length 0" Band bug.
    Fixes #55

--- src/unBand/App.xaml.cs
+++ src/unBand/App.xaml.cs
@@ -17,6 +17,15 @@ namespace unBand
         {
             Telemetry.TrackException(e.Exception);
 
+            if (e.Exception is ArgumentException)
+            {
+                if (e.Exception.Message.Contains("name cannot have length 0"))
+                {
+                    MessageBox.Show("Your Band is hitting a Tile bug that unfortunately unBand cannot work around.\n\nTo fix this please remove all of your tiles through the Phone app and the readd them back, this should fix this error. Sadly, unBand will now close.\n\nIf it doesn't, please open a bug at https://github.com/nachmore/unBand/issues with the contents of this message (hit Ctrl+C now to copy)\n\n" + e.Exception.ToString());
+                    return;
+                }
+            }
+
             MessageBox.Show("An unhandled exception occurred - sorry about that, we're going to have to crash now :(\n\nYou can open a bug with a copy of this crash: hit Ctrl + C right now and then paste into a new bug at https://github.com/nachmore/unBand/issues.\n\n" + e.Exception.ToString(),
                 "Imminent Crash", MessageBoxButton.OK, MessageBoxImage.Exclamation);
         }
